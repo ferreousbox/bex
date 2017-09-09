b2fdaa6e764ab03cc441b5368a95bc98d0564f68
铁木箱子 <biqiang.ma@gmail.com>
2017-09-09 14:02:31 +0800

    Update BEXBridge.sol

--- contract/trade/BEXBridge.sol
+++ contract/trade/BEXBridge.sol
@@ -1,8 +1,37 @@
 pragma solidity ^0.4.16;
 
-/**
- * @title 
- */
+// bridge for user account contract and bank contract
 contract BEXBridge {
-
+    address owner;
+    mapping(string => address) tokens;
+    address bankAddr;
+    
+    function BEXBridge() {
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwnerr() {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    function getBankAddr() constant returns (address) {
+        return bankAddr;
+    }
+    
+    function setBankAddr(address _addr) onlyOwner returns (bool) {
+        bankAddr = _addr;
+    }
+    
+    function addToken(string _name, address _addr) onlyOwner returns (bool) {
+        tokens[_name] = _addr;
+    }
+    
+    function delToken(string _name) onlyOwner returns (bool) {
+        del tokens[_name];
+    }
+    
+    function getToken(string _name) constant returns (address) {
+        return tokens[_name];
+    }
 }
