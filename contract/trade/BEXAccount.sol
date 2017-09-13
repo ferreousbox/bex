a12057e489bcd60d7e777be3c987a38aa7cc814f
铁木箱子 <biqiang.ma@gmail.com>
2017-09-14 00:00:28 +0800

    Create BEXAccount.sol

---
+++ contract/trade/BEXAccount.sol
@@ -0,0 +1,45 @@
+pragma solidity ^0.4.16;
+
+contract BEXAccount {
+    address public withdrawAddr;
+    address public tradeAddr;
+    address public owner;
+    bool public activated;
+    uint public activateTime;
+    address public activateAddr;
+    uint public activateFund;
+    
+    function BEXAccount(address _withdrawAddr, address _tradeAddr) {
+        require(_withdrawAddr != 0);
+        require(_tradeAddr != 0);
+        owner = msg.sender;
+        withdrawAddr = _withdrawAddr;
+        tradeAddr = _tradeAddr;
+        activated = false;
+    }
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    function() payable {
+        activate();
+    }
+    
+    function activate() payable {
+        require(activated == false);
+        require(msg.value > 0);
+        // return the fund
+        if (!withdrawAddr.call.value(msg.value)()) throw;
+        activated = true;
+        activateTime = now;
+        activateAddr = msg.sender;
+        activateFund = msg.value;
+    }
+    
+    function depositToBank() onlyOwner {
+        
+    }
+    
+}
